c7ca726c3c6b0c645d31cddfc70bee959325e823
= <=>
2025-03-12 17:33:07 +0100

    Dodano klasę Item z właściwościami i metodą toString
    Wprowadzono nowe pola `id`, `weight`, `price` oraz `ratio` w klasie `Item`. Dodano konstruktor do inicjalizacji `id` oraz losowania wartości dla `weight` i `price`. Zaimplementowano metodę `toString`, która wyświetla szczegóły obiektu w konsoli.

---
+++ PWR_dotNER_Pr1/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PWR_dotNER_Pr1{
+    class Item {
+        private int id;
+        public int Id{
+            get { return this.id; }
+        }
+        private int weight;
+        public int Weight{
+            get { return this.Weight; }
+        }
+        private int price;
+        public int Price{
+            get { return this.price; }
+        }
+        private float ratio;
+        public float Ratio{
+            get { return this.ratio; }
+        }
+        public Item(int id, int seed){
+            Random random = new Random(seed);
+            this.id = id;
+            this.weight = random.Next(10);
+            this.price = random.Next(10);
+        }
+        void toString(){
+            Console.WriteLine($"id: {this.id} weight: {this.weight} price: {this.price} price to weight ratio : {this.ratio}");
+        }
+    }
+}
